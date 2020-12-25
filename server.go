b318eeb6b2e06fc0f0f53d93b36433aa20b1159c
Luiz Fernando <luizepauloxd@gmail.com>
2020-12-25 06:07:30 -0300

    feat: adds authentication and connectivity to the database

--- server.go
+++ server.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/LFSCamargo/graphql-go-boilerplate/auth"
+	"github.com/LFSCamargo/graphql-go-boilerplate/database"
 	"github.com/LFSCamargo/graphql-go-boilerplate/graph"
 	"github.com/LFSCamargo/graphql-go-boilerplate/graph/generated"
+	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
@@ -21,9 +24,19 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	database.Connect()
 
+	router := chi.NewRouter()
+
+	router.Use(auth.Middleware())
+	router.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
+	router.Handle("/graphql", srv)
+
+	log.Printf("Server exposed at http://localhost:%s/graphql", port)
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	err := http.ListenAndServe(":"+port, router)
+
+	if err != nil {
+		panic(err)
+	}
 }
